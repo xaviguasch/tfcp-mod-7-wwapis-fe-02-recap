9eeb6efa0a4001267c62677fac583ee68b063319
xaviguasch <xaviguasch@gmail.com>
2020-08-14 15:25:53 +0200

    added uploading of multiple files

--- index.js
+++ index.js
@@ -49,6 +49,34 @@
 
 // Create a variable for the form element. Then create an event listener for submit. Prevent the default behavior. Post a FormData object to https://httpbin.org/post, then console log the JSON response.
 
+// const uploadForm = document.getElementById('uploadForm')
+
+// uploadForm.addEventListener('submit', function (e) {
+//   e.preventDefault()
+
+//   uploadFile(this)
+// })
+
+// async function uploadFile(data) {
+//   const formData = new FormData(data)
+
+//   const options = {
+//     method: 'POST',
+//     body: formData,
+//   }
+
+//   const uploadPromise = await fetch('https://httpbin.org/post', options)
+
+//   if (uploadPromise.ok) {
+//     const uploadResp = await uploadPromise.json()
+//     console.log(uploadResp)
+//   } else {
+//     console.error(uploadPromise.status)
+//   }
+// }
+
+//  UPLOADING MULTIPLE FILES
+
 const uploadForm = document.getElementById('uploadForm')
 
 uploadForm.addEventListener('submit', function (e) {
@@ -58,7 +86,12 @@ uploadForm.addEventListener('submit', function (e) {
 })
 
 async function uploadFile(data) {
-  const formData = new FormData(data)
+  const formData = new FormData()
+  const files = data.querySelector('input[type="file"]').files
+
+  for (let i = 0; i < files.length; i++) {
+    formData.append(`fileInput_${i}`, files[i])
+  }
 
   const options = {
     method: 'POST',
@@ -69,7 +102,7 @@ async function uploadFile(data) {
 
   if (uploadPromise.ok) {
     const uploadResp = await uploadPromise.json()
-    console.log(uploadResp)
+    console.log(uploadResp.files)
   } else {
     console.error(uploadPromise.status)
   }
