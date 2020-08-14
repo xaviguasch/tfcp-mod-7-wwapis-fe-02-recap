74ddaf1b8658a85638e5370942f93601a90929e1
xaviguasch <xaviguasch@gmail.com>
2020-08-14 15:57:39 +0200

    added multiple fetch requests exercise, part 2

--- index.js
+++ index.js
@@ -114,13 +114,20 @@
 
 // WORKING WITH MULTIPLE FETCH REQUESTS
 
+// Below the authors name, add a new paragraph to the HTML that contains the company that the user works for.
+
 async function getPost() {
-  const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const [postPromise, userPromise] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts'),
+    fetch('https://jsonplaceholder.typicode.com/users'),
+  ])
 
   const posts = await postPromise.json()
+  const users = await userPromise.json()
 
   const html = `
   <h3>${posts[0].title}</h3>
+      <h5>${users.filter((user) => user.id === posts[0].userId)[0].name}</h5>
   <p>${posts[0].body}</p>
 `
 
