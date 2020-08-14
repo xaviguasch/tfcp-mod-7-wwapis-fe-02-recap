e707f5e6e66c94b756cf7c6b324b9c2da8538e84
xaviguasch <xaviguasch@gmail.com>
2020-08-14 12:51:39 +0200

    added submit form data challenge

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // Use Fetch with this URL, https://jsonplaceholder.typicode.com/posts/1, to get a single blog post and render the blog title and body to the page.
 
-async function getPost() {
-  const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts/1')
-  const post = await postPromise.json()
-  const html = `
-      <h3>${post.title}</h3>
-      <p>${post.body}</p>
-  `
+// async function getPost() {
+//   const postPromise = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   const post = await postPromise.json()
+//   const html = `
+//       <h3>${post.title}</h3>
+//       <p>${post.body}</p>
+//   `
 
-  document.body.innerHTML = html
-}
+//   document.body.innerHTML = html
+// }
 
-getPost()
+// getPost()
+
+///////////////////////////////////////////////////////////////////////////
+
+//  SUMBIT FORM DATA
+
+const userForm = document.getElementById('userForm')
+
+userForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  const formData = new FormData(this)
+  const options = {
+    method: 'POST',
+    body: formData,
+  }
+
+  // Write the Fetch statement using https://httpbin.org/post along with the options. Then console log the JSON response.
+})
